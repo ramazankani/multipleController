37a8b6b6ac65b4789b45b82e82ebc6e8f6ab94de
necipfazilergun <officialnecip@gmail.com>
2018-04-12 21:01:08 +0300

    Updated using get-controller
    -instead of using c1 and c2 lists, now we are getting its previous controller address(ip:port) and connect to the other controller.

--- manualController.py
+++ manualController.py
@@ -5,32 +5,24 @@ import os
 import commands
 
 bridge_list = ['s1','s2','s3','s4','s5','s6','s7','s8','s9','s10']
-c1 = ['s1','s3','s4','s5','s6']
-c2 = ['s2','s7','s8','s9','s10']
 
 while True:
 
 	time.sleep(3)
-	#hostname = "127.0.0.1:6633"
-	#response = os.system("ping -c 1 " + hostname)
 
 	reponse = ""
 	for i in range(1,10):
-		response = commands.getstatusoutput("sudo ovs-vsctl get controller "+ bridge_list[i] + " is_connected")
-		#print(response)
-		if response[1] == "false":
-			#print[bridge_list[i]]
-			if bridge_list[i] in c1:
+		getcontr = commands.getstatusoutput("sudo ovs-vsctl get-controller "+ bridge_list[i])
+		if(getcontr[1] == "tcp:127.0.0.1:6633"):
+			response = commands.getstatusoutput("sudo ovs-vsctl get controller "+ bridge_list[i] + " is_connected")
+			if response[1] == "false":
 				os.system("sudo ovs-vsctl set-controller " +bridge_list[i]+ " tcp:127.0.0.1:6639")
-				print(bridge_list[i]+" connected c2")
-			elif bridge_list[i] in c2:
+				print("switch :: "+bridge_list[i]+" is connected to controller:c2")
+		elif(getcontr[1] == "tcp:127.0.0.1:6639"):
+			response = commands.getstatusoutput("sudo ovs-vsctl get controller "+ bridge_list[i] + " is_connected")
+			if response[1] == "false":
 				os.system("sudo ovs-vsctl set-controller " +bridge_list[i]+ " tcp:127.0.0.1:6633")
-				print(bridge_list[i]+" connected c1")
-		
-	#	if response==true:
-	#	print("ping geliyor abi")
-	#else: 
-	#	print("gelmiyor abi")
+				print("switch :: "+bridge_list[i]+" is connected to controller:c1")
 
 	
 	
